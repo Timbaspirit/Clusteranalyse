25c5426f1c317b56a1acd2a5dbe8455844bf3782
Vedran Alagic <alagicvn@stud55.mathematik.uni-stuttgart.de>
2019-07-21 17:53:50 +0200

    v2, Iteration durch alle Werte hinzugefügt

--- cluster.py
+++ cluster.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jun 25 11:51:38 2019
@@ -9,6 +9,7 @@ import numpy as np
 import math
 import numpy.linalg as LA
 import timeit
+import itertools
 
 
 def dissect(data, delta):
@@ -42,16 +43,16 @@ def depthFirstSearch(M_rho, B, keylist, key, d, taumult):
       #suboptimal routine disregarding single points within boxes since harder
       
       newcomp = M_rho.pop(key)
-      print("Größe der hinzuzufügenden Komp: " + str(newcomp.shape))
+#      print("Größe der hinzuzufügenden Komp: " + str(newcomp.shape))
       B = np.vstack((B, newcomp))
-      print("Zsmkomp nach hinzufügen von Kasten" + str(key) + ":" + str(B.shape))
+#      print("Zsmkomp nach hinzufügen von Kasten" + str(key) + ":" + str(B.shape))
       keylist.append(key)
       
       keys = list(M_rho.keys())
       for candidatekey in keys:
             diff = LA.norm(np.asarray(key)-np.asarray(candidatekey),np.inf)
             if (diff <= taumult/2) and (candidatekey in M_rho.keys()):
-                  print("kommt von " + str(key))
+#                  print("kommt von " + str(key))
                   B = depthFirstSearch(M_rho, B, keylist, candidatekey, d, taumult)
             
 #      for i in range(d):
@@ -80,7 +81,7 @@ def cluster(name, epsmult, delta, taumult):
       testdata = np.ndarray(shape=(0,d))
       for key in dissection.keys():
             testdata = np.vstack((testdata, dissection[key]))
-      print("dissection hat die Form " + str(testdata.shape))
+#      print("dissection hat die Form " + str(testdata.shape))
       
       B_delta = 0
       q = n * ((2*delta)**d) #constant for calculation of density function
@@ -92,8 +93,10 @@ def cluster(name, epsmult, delta, taumult):
                    
       eps = math.sqrt(B_delta / (n*(delta**d))) * epsmult
       rho = 0
+#      rho = 604 / (n*(delta**d))
       MM = 1
       while MM == 1:
+#            print("j = " + str(int(rho*(n*(delta**d)))))
             B = {}
             keylist = {}
             k = 1
@@ -101,17 +104,18 @@ def cluster(name, epsmult, delta, taumult):
             testdata = np.ndarray(shape=(0,d))
             for key in M_rho.keys():
                   testdata = np.vstack((testdata, M_rho[key]))
-            print("M_rho hat die Form " + str(testdata.shape))
+#            print("M_rho hat die Form " + str(testdata.shape))
             keys = list(M_rho.keys())
-            print(keys)
+#            print(keys)
             for key in keys:
-                  print("__________ k = " + str(k) + "__________")
+#                  print("__________ k = " + str(k) + "__________")
                   if key in M_rho.keys():
                         B[k] = np.ndarray(shape=(0,d))
                         keylist[k] = []
                         B[k] = depthFirstSearch(M_rho, B[k], keylist[k], key, d, taumult)
-                        print(B[k])
+#                        print(B[k])
                         k += 1
+            removedB = {}
             for i in range(1,k):
                   remove = 1
                   for key in keylist[i]:
@@ -119,28 +123,63 @@ def cluster(name, epsmult, delta, taumult):
                               remove = 0
                               break
                   if remove == 1:
-                        B.pop(i)
+#                        if len(B.keys()) == 1:
+#                              MM_data = np.ndarray(shape=(0,d + 1))
+#                              for key in B.keys():
+#                                    classvector = np.ones((B[key].shape[0], 1))
+#                                    print("Daten des Schlüssels " + str(key) + ":")
+#                                    print(B[key])
+#                                    newdata = np.hstack((classvector, B[key]))
+#                                    MM_data = np.vstack((MM_data, newdata))
+                        removedB[i] = B.pop(i)
             MM = len(B.keys())
             rho += 1 / (n*(delta**d))
-            if MM != 1:
-                  print("Anzahl der Äquivalenzklassen: " + str(MM))
-                  print("Anzahl der Durchläufe: " + str(int(rho*(n*(delta**d)))))
+#            if MM != 1:
+#                  print("Anzahl der Äquivalenzklassen: " + str(MM))
+#                  print("Anzahl der Durchläufe: " + str(int(rho*(n*(delta**d)))))
              
       MM_data = np.ndarray(shape=(0,d + 1))
+      if MM > 1:
+            final_cluster = B
+      elif MM == 0:
+            final_cluster = removedB
       i = 1
-      for key in B.keys():
-            classvector = np.ones((B[key].shape[0], 1))*i
-            print(B[key])
-            newdata = np.hstack((classvector, B[key]))
+      for key in final_cluster.keys():
+            classvector = np.ones((final_cluster[key].shape[0], 1))*i
+#                 print("Daten des Schlüssels " + str(key) + ":")
+#                 print(B[key])
+            newdata = np.hstack((classvector, final_cluster[key]))
             MM_data = np.vstack((MM_data, newdata))
             i += 1
-      np.savetxt(name + ".result.csv", MM_data, delimiter=",", fmt="%1.4f")      
-
-
-start = timeit.default_timer()
-
-cluster("bananas-1-4d", 1, 0.05, 2)
-
-stop = timeit.default_timer()
-print("Time: " + str(stop - start))            
+      
+#      else:
+#            print("Zahl der Zsmhangskomponenten war nicht >1 oder == 0")
             
+      output_name = "out/" + name + "." + str(epsmult) + "." + str(delta) + \
+      "." + str(taumult) + ".result.csv"
+      np.savetxt(output_name, MM_data, delimiter=",", fmt="%1.4f")      
+      
+#      plot_data = {}
+#      colorlist = ['b', 'g', 'r', 'c','m', 'y', 'k', 'w']
+#      for key in final_cluster:
+      
+input_name = "cod-rna.5000"
+      
+#valuelist = [[1,1.5,2],[.2,.1,.06,.05,.04],[2,4]]
+valuelist = [[1],[0.04],[2]]
+for [epsmult, delta, taumult] in itertools.product(*valuelist):
+      start = timeit.default_timer()
+
+      cluster(input_name, epsmult, delta, taumult)
+
+      stop = timeit.default_timer()
+      
+#      print(input_name + "." + str(epsmult) + "." + str(delta) + \
+#      "." + str(taumult) + ", Time: " + str(stop - start))  
+          
+      time_file_name = "out/time.txt"
+      with open(time_file_name, "a") as time_file:
+            line = input_name + ", " + "epsmult = " + str(epsmult) + \
+                  ", delta = " + str(delta) + ", taumult = " + str(taumult) + \
+                  ". Time: " + str(stop-start) + "\n"
+            time_file.write(line)            
